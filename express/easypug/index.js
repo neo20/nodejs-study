497af4d525dbc7638ceeaa82f81fc8b206094b93
neo20 <350176528@qq.com>
2019-06-10 22:24:48 +0800

    day8

---
+++ express/easypug/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const pug = require('pug')
+const fs = require('fs')
+
+var app = express()
+
+app.use('/static', express.static('public'))
+
+app.get('/*', function(req, res) {
+	let pugfile = 'template' + req.path + '.pug'
+	fs.exists(pugfile,function(exists){
+		if (exists) {
+			let pugfunc = pug.compileFile(pugfile)
+			res.send(pugfunc())
+		} else {
+			res.send( 'path : '+req.path + ' not found' )
+		}
+	})
+	
+})
+app.listen(3000, () => console.log('EasyPug app listening on port 3000!'))
