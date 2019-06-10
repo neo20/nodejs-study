d94dc717329e550486924ba4333c9708c60e5b8e
neo20 <350176528@qq.com>
2019-06-10 21:35:36 +0800

    day8

--- express/helloworld/index.js
+++ express/helloworld/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.use('/static', express.static('public'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
